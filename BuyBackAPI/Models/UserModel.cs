38bf641e6d6294072993363623c7115eefaade18
E-Creator <just4otherwebsite@gmail.com>
2019-02-26 23:05:26 +0530

    Initial Commit

---
+++ BuyBackAPI/Models/UserModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BuyBackAPI.Models
+{
+    [DataContract]
+    public class UserModel
+    {
+        [DataMember(Name = "id")]
+        public int? Id { get; set; }
+
+        [DataMember(Name = "firstname")]
+        public string Firstname { get; set; }
+
+        [DataMember(Name = "middlename")]
+        public string Middlename { get; set; }
+
+        [DataMember(Name = "lastname")]
+        public string Lastname { get; set; }
+
+        [DataMember(Name = "dateofbirth")]
+        public DateTime? DateOfBirth { get; set; }
+
+        [DataMember(Name = "age")]
+        public int? Age { get; set; }
+
+        [DataMember(Name = "gender")]
+        public string Gender { get; set; }
+
+        [DataMember(Name = "mobileno")]
+        public string Mobileno { get; set; }
+
+        [DataMember(Name = "emailaddress")]
+        public string Emailaddress { get; set; }
+
+        [DataMember(Name = "bio")]
+        public string Bio { get; set; }
+
+        [DataMember(Name = "roleid")]
+        public int? Roleid { get; set; }
+
+        [DataMember(Name = "islocked")]
+        public int? IsLocked { get; set; }
+
+        [DataMember(Name = "profileimage")]
+        public int? ProfileImage { get; set; }
+    }
+}
